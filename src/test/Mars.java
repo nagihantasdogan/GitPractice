b9d49ad6b74ce83cfeec26651ea0d07276f05792
nagihantasdogan <56852858+nagihantasdogan@users.noreply.github.com>
2020-02-04 21:05:44 -0500

    Update Mars.java
    this  my remote changers

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -1,5 +1,14 @@
 package test;
 
 public class Mars {
-
+  
+	public static void main(String[] args) {
+		System.out.println("Hello World ");
+		
+		
+		
+		
+		
+		System.out.println("Hello2");
+  }
 }
